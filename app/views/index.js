244f8a51aefce3bdf7ff056e8c8096a6ecde35ec
Dylan Piercey <pierceydylan@gmail.com>
2017-08-29 09:49:07 -0700

    Update deps

--- app/views/index.js
+++ app/views/index.js
@@ -7,11 +7,17 @@ import notFound from './404.marko'
 
 export default router()
   .get(page
+    .html({ lang: 'en' })
     .title('Marko BoilerPlate')
     .link({ rel: 'stylesheet', href: '/index.css' })
     .script({ async: true, src: '/polyfill.js' })
     .script({ async: true, src: '/index.js' })
   )
+  .use(({ locals, req }, next) => {
+    locals.pathname = req.pathname
+    locals.query = req.query
+    return next()
+  })
   .get('/', render(homePage))
   .get('/other', render(otherPage))
   .get(render(notFound))
